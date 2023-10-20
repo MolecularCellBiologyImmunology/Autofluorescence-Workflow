3fdd854023dc949724e5f9d571dc933e87a8d8b3
Michael de Kok <45708312+MikedeKokkie@users.noreply.github.com>
2023-10-20 18:36:16 +0200

    Update SpectroFlo-Find-Unique-Spectra
    Added checks + user prompts for identical similarity scores

--- SpectroFlo-Find-Unique-Spectra.R
+++ SpectroFlo-Find-Unique-Spectra.R
@@ -1,6 +1,7 @@
 # Script to Define Unique Spectra from SpectroFlo Similarity Matrix
 # Author: Michael de Kok
 # Date: 09-08-2022
+# Update: 18-10-2023
 
 # Load SpectroFlo Halfmatrix. We assume up to 100 columns then remove empty ones.
 halfmatrix <- read.csv(choose.files(), 
@@ -42,23 +43,45 @@ summary <- data.frame("Spectrum_Name" = colnames(fullmatrix),
 summary <- summary[order(summary[ , 3], summary[ , 2], decreasing = F),]
 
 # Now, we test which Spectra will remain Unique, going from top to bottom:
-for (spectrum in summary$Spectrum_Name) {
+for (i in 1:dim(summary)[1]) {
+  keep <- 0
+  spectrum1 <- summary[i,"Spectrum_Name"]
   
   # Only consider Similar Spectra Non-Unique if this Spectrum is still Unique
-  if (summary[spectrum,"Unique"] == TRUE) {
-  
+  if (summary[i,"Unique"] == TRUE) {
+    
     # Only consider Similar Spectra Non-Unique if there is at least 1 match
-    if (summary[spectrum,"Number_Identical"] > 0) {    
-    
-      # Take note which Spectra are identical to this Spectra
-      ident_spectra <- similarity_logical[spectrum,]
-      ident_spectra <- ident_spectra[-which(names(ident_spectra) == spectrum)]
-      ident_spectra <- names(ident_spectra[ident_spectra])
+    if (summary[i,"Number_Identical"] > 0) {    
       
-      # Exclude those identical spectra from the list of Unique Spectra
-      summary[ident_spectra,"Unique"] <- FALSE
-    
-}}}
+      # In the case of identical Similarity Scores between still-unique Spectra
+      if ((summary[i,"Similarity_Sum"] == summary[(i+1),"Similarity_Sum"]) &
+        (summary[i,"Unique"] == TRUE & summary[i+1,"Unique"] == TRUE)) {
+        
+        # Print a warning and make the user choose between Spectra to proceed
+        spectrum2 <- summary[i+1,"Spectrum_Name"]
+        warning(paste("Identital Similarity Sums",summary[(i),"Similarity_Sum"], 
+                      "for spectra", spectrum1, "and", spectrum2, "detected.", 
+                      sep = " "), immediate. = TRUE)
+        keep <- readline(paste("To proceed, pick a Spectrum to prioritize:",
+                               "\n1:", spectrum1, "\n2:", spectrum2, 
+                               "\nType either 1 or 2 and press ENTER. "))
+      } 
+      
+      # In the case of non-idential Similarity Scores or if spectrum 1 was kept
+      if (keep < 2) {
+        
+        # Take note which Spectra are identical to this Spectra
+        ident_spectra <- similarity_logical[spectrum1,]
+        ident_spectra <- ident_spectra[-which(names(ident_spectra)==spectrum1)]
+        ident_spectra <- names(ident_spectra[ident_spectra])
+        # Exclude those identical spectra from the list of Unique Spectra
+        summary[ident_spectra,"Unique"] <- FALSE
+        
+        # In the case of identical Similarity Scores and spectrum 2 was kept
+      } else {summary[spectrum1,"Unique"] <- FALSE}
+    }
+  }
+}
 
 # Whatever Spectra remain, must be Unique. Print those Spectra as Results.
 unique_spectra <- rownames(summary)[summary$Unique]
